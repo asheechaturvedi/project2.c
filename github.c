57efdd31a3f73352b7572a739f19040176ae9813
asheechaturvedi <68819138+asheechaturvedi@users.noreply.github.com>
2020-07-26 21:32:08 +0530

    Add files via upload

---
+++ github.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	double a, b;
+	printf("Enter a: ");
+	scanf("%lf", &a);
+	printf("Enter b: ");
+	scanf("%lf", &b);
+	printf("After Swapping, a = %lf/n", b);
+	printf("After Swapping, b = %lf/n", a);
+	return 0;
+}
